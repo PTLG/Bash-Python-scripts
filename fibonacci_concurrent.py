e1f8a1e9924457805ebbf71d252962a31eb27759
Zubel Tomasz <kontowykopkowe@gmail.com>
2018-01-18 13:51:20 +0100

    first steps into learning something about python concurrency, btw. it will be very helpful if I could check this subject also on the bash ;p

---
+++ fibonacci_concurrent.py
@@ -0,0 +1,11 @@
+import threading
+import _thread
+"""
+Importing modules for handling the threading/concurrency. The threading
+module is more "easy-to-use" interface to the _thread module. If programmers
+needs more complexive methods or solutions to handle concurrency/threading
+problems he should use _thread module. In other ways - threading should be
+the first option to choose.
+"""
+
+
